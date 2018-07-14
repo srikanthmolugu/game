b8b5f81426ac752919a0ada273708f142cf3a91d
srikanthmolugu <39045117+srikanthmolugu@users.noreply.github.com>
2018-07-14 10:49:58 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    var button = ""
+    //var answer="";
+    //var e=getElementByClassName("content").$(getElementById("button1")).value;
+    $('.button').click(function(e){ 
+        button += $(this)[0].value;
+        console.log(button);
+        document.getElementById("answer1").innerHTML=button;
+     })
+     $('#submit').click(function(){
+         if(button=="ZUPA"){
+             alert("u r done");
+             window.location="../html/level2.html";
+         }
+         else{
+             alert("game over");
+             window.location="../html/welcome1.html";
+         } 
+    } )
+     
+});
